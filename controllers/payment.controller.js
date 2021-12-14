13e442cd166fa1cc82cf31dab9eff4560aaa691b
Joel Montesano <joel.montesanos@gmail.com>
2021-12-13 22:35:37 -0600

    feed: payment controller completed

--- controllers/payment.controller.js
+++ controllers/payment.controller.js
@@ -1,6 +1,6 @@
 const PAYMENT_FILE_PATH = '../payment-generated.txt';
 const faker = require('faker');
-const fs = require('fs');
+const readline = require('readline');
 const LINE_ENDING = require('os').EOL;
 
 const dataflag = 7777777777;
@@ -17,7 +17,30 @@ module.exports = {
 
     applyDiscount: function (req, res) {
         //debera de restar una cantidad a cada precio en payment-generated.txt
-        res.json({ message: ""});
+        let n=this.getinfo.length;
+        for(var i=0; i>n;i++ ){
+            var discount= (this.getinfo[n]-(this.getinfo[n]*.10))
+            fs.writeline(PAYMENT_FILE_PATH, discount || '', 'utf8',(err => {
+                if(err) return reject();
+                else resolve();
+            }));
+        }
+        res.json({ message: "Discount applied"});
+    },
+
+    getinfo = () =>{
+        this.readFile(PAYMENT_FILE_PATH).then(data => {
+            return compact(data.split(os.EOL));
+        });
+    },
+
+    readFile = (path) => function (req, res ){
+        return new Promise((resolve, reject) => {
+            fs.readFile(path, {encoding: 'utf-8'}, (err, data) => {
+                if (err) reject(err);
+                else resolve(data);
+            });
+        });
     },
 
     getPromos: function (req, res) {
